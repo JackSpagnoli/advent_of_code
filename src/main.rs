c9bfc43081a11a8bbafa6b4e7ba097b8588c6069
Jack Spagnoli <jackspagnoli@gmail.com>
2023-12-01 10:57:47 +0000

    repo setup

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::time::Instant;
+
+use aoc2023::*;
+
+fn main() {
+    let problems: Vec<(&dyn Fn() -> Whole128, Whole128, &str)> = vec![
+    ];
+
+    problems.iter().for_each(check_problem)
+}
+
+fn check_problem(problem: &(&dyn Fn() -> Whole128, Whole128, &str)) {
+    let problem_function = problem.0;
+    let expected = &problem.1;
+    let problem_name = problem.2;
+
+    let now = Instant::now();
+
+    let actual_result = problem_function();
+
+    let duration = now.elapsed().as_secs_f32();
+    println!("Problem {problem_name} took {duration} seconds");
+
+    match (actual_result, expected) {
+        (Whole128::Signed(actual), Whole128::Signed(expected)) => assert_eq!(actual, *expected),
+        (Whole128::Unsigned(actual), Whole128::Unsigned(expected)) => assert_eq!(actual, *expected),
+        _ => panic!("Expected return type does not match actual return type"),
+    }
+}
