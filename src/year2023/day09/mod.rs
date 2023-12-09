bd577246ee92e6be11b546462445c74bb09c8e4d
Jack Spagnoli <jack@spagnoli.co.uk>
2023-12-09 13:43:06 +0000

    refactors

--- src/year2023/day09/mod.rs
+++ src/year2023/day09/mod.rs
@@ -9,10 +9,10 @@ pub mod task1 {
 }
 
 pub mod task2 {
-    use super::exum_previous_terms;
+    use super::sum_previous_terms;
 
     pub fn ans() -> i128 {
-        exum_previous_terms("resources/2023/day09/input")
+        sum_previous_terms("resources/2023/day09/input")
     }
 }
 
@@ -20,8 +20,17 @@ fn sum_next_terms(file: &str) -> i128 {
     parse_file(file).into_iter().map(predict_next).sum()
 }
 
-fn exum_previous_terms(file: &str) -> i128 {
-    parse_file(file).into_iter().map(predict_previous).sum()
+fn sum_previous_terms(file: &str) -> i128 {
+    parse_file(file)
+        .into_iter()
+        .map(reverse_vec)
+        .map(predict_next)
+        .sum()
+}
+
+fn reverse_vec(mut nums: Vec<i128>) -> Vec<i128> {
+    nums.reverse();
+    nums
 }
 
 fn parse_file(file: &str) -> Vec<Vec<i128>> {
@@ -50,19 +59,6 @@ fn predict_next(nums: Vec<i128>) -> i128 {
     last + next_diff
 }
 
-fn predict_previous(nums: Vec<i128>) -> i128 {
-    let diffs: Vec<i128> = nums.windows(2).map(|w| w[1] - w[0]).collect();
-
-    if diffs.iter().all(|n| *n == 0) {
-        return nums[0];
-    }
-
-    let first = nums[0];
-    let former_diff = predict_previous(diffs);
-
-    first - former_diff
-}
-
 #[cfg(test)]
 mod tests {
     use super::parse_file;
@@ -83,17 +79,5 @@ mod tests {
     fn test_sum_next_terms() {
         let sum = super::sum_next_terms("resources/2023/day09/test_input");
         assert_eq!(sum, 114);
-    }
-
-    #[test]
-    fn test_predict_previous() {
-        let sequences: Vec<Vec<i128>> = parse_file("resources/2023/day09/test_input");
-
-        let expected_previous = vec![-3, 0, 5];
-
-        for (seq, expected) in sequences.into_iter().zip(expected_previous) {
-            let next = super::predict_previous(seq);
-            assert_eq!(next, expected);
-        }
     }
 }
