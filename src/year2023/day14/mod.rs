ee3cd6271d03bc89c980316cd314a5eb1e71c444
Jack Spagnoli <jackspagnoli@gmail.com>
2023-12-14 14:24:41 +0000

    'optimized'

--- src/year2023/day14/mod.rs
+++ src/year2023/day14/mod.rs
@@ -1,4 +1,8 @@
-use std::{cmp::Ordering, collections::HashMap};
+use std::{
+    cmp::Ordering,
+    collections::HashMap,
+    ops::{Index, IndexMut},
+};
 
 pub mod task1 {
     use super::total_load;
@@ -17,139 +21,49 @@ pub mod task2 {
 }
 
 fn total_load(file: &str) -> u128 {
-    let grid = parse_file(file);
-    let rolled_grid = roll_north(grid);
-
-    grid_load(rolled_grid)
+    parse_file(file).roll_north().grid_load()
 }
 
 fn billion_cycles_load(file: &str) -> u128 {
     let grid = parse_file(file);
-    let rolled_grid = billion_cycles(grid);
+    billion_cycles(grid).grid_load()
 
-    grid_load(rolled_grid)
-}
-
-fn grid_load(grid: Grid) -> u128 {
-    grid
-        .into_iter()
-        .rev()
-        .enumerate()
-        .map(|(index, row)| {
-            (row.into_iter()
-                .filter(|tile| *tile == Tile::Rolling)
-                .count()
-                * (index + 1)) as u128
-        })
-        .sum()
 }
 
 fn billion_cycles(grid: Grid) -> Grid {
     let number_of_cycles = 1_000_000_000;
 
-    let mut memory: HashMap<Grid, usize> = HashMap::new();
+    let mut memory: HashMap<String, usize> = HashMap::new();
 
     let mut current_grid = grid;
     let mut cycles = 0;
-    memory.insert(current_grid.clone(), cycles);
+    memory.insert(current_grid.string(), cycles);
 
     loop {
-        current_grid = cycle(current_grid);
+        current_grid = current_grid.cycle();
         cycles += 1;
 
         if cycles == number_of_cycles {
             return current_grid;
         }
 
-        if memory.contains_key(&current_grid) {
-            let previous_cycles = *memory.get(&current_grid).unwrap();
+        if memory.contains_key(&current_grid.string()) {
+            let previous_cycles = *memory.get(&current_grid.string()).unwrap();
             let loop_length = cycles - previous_cycles;
 
             let remaining_cycles = number_of_cycles - cycles;
             let mod_cycles = remaining_cycles % loop_length;
 
-            return memory
+            let billionth_grid_string = memory
                 .into_iter()
                 .find(|(_, index)| *index == mod_cycles + previous_cycles)
                 .unwrap()
                 .0;
+            return parse_string(billionth_grid_string);
         }
 
-        memory.insert(current_grid.clone(), cycles);
+        memory.insert(current_grid.string(), cycles);
     }
-}
-
-fn cycle(grid: Grid) -> Grid {
-    roll_east(roll_south(roll_west(roll_north(grid))))
-}
-
-fn roll_north(grid: Grid) -> Grid {
-    let mut transposed_grid = transpose(&grid);
-
-    transposed_grid = transposed_grid.into_iter().map(roll_column).collect();
-
-    transpose(&transposed_grid)
-}
-
-fn roll_south(grid: Grid) -> Grid {
-    let mut transposed_grid = transpose(&grid);
-
-    transposed_grid = transposed_grid
-        .into_iter()
-        .map(|mut column| {
-            column.reverse();
-            column
-        })
-        .map(roll_column)
-        .map(|mut column| {
-            column.reverse();
-            column
-        })
-        .collect();
-
-    transpose(&transposed_grid)
-}
-
-fn roll_west(grid: Grid) -> Grid {
-    grid.into_iter().map(roll_column).collect()
-}
-
-fn roll_east(grid: Grid) -> Grid {
-    grid.into_iter()
-        .map(|mut column| {
-            column.reverse();
-            column
-        })
-        .map(roll_column)
-        .map(|mut column| {
-            column.reverse();
-            column
-        })
-        .collect()
-}
-
-fn roll_column(mut column: Vec<Tile>) -> Vec<Tile> {
-    let n = column.len();
-    (0..n - 1).for_each(|i| {
-        (0..n - i - 1).for_each(|j| {
-            if sort_tiles(&column[j], &column[j + 1]) == Ordering::Greater {
-                column.swap(j, j + 1)
-            }
-        })
-    });
-    column
-}
-
-fn transpose(grid: &Grid) -> Grid {
-    let mut transposed_pattern = vec![vec![Tile::Empty; grid.len()]; grid[0].len()];
-
-    grid.iter().enumerate().for_each(|(j, row)| {
-        row.iter().enumerate().for_each(|(i, tile)| {
-            transposed_pattern[i][j] = *tile;
-        })
-    });
-
-    transposed_pattern
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
@@ -158,23 +72,163 @@ enum Tile {
     Static,
     Rolling,
 }
-type Grid = Vec<Vec<Tile>>;
+impl From<char> for Tile {
+    fn from(c: char) -> Self {
+        match c {
+            '.' => Tile::Empty,
+            '#' => Tile::Static,
+            'O' => Tile::Rolling,
+            _ => panic!("Invalid character in input file"),
+        }
+    }
+}
+struct Grid {
+    tiles: Vec<Vec<Tile>>,
+    transposed: bool,
+    reversed: bool,
+}
+impl Grid {
+    fn transpose(mut self) -> Self {
+        self.transposed = !self.transposed;
+        self
+    }
+    fn reverse(mut self) -> Self {
+        self.reversed = !self.reversed;
+        self
+    }
+    fn width(&self) -> usize {
+        if self.transposed {
+            self.tiles.len()
+        } else {
+            self.tiles[0].len()
+        }
+    }
+    fn height(&self) -> usize {
+        if self.transposed {
+            self.tiles[0].len()
+        } else {
+            self.tiles.len()
+        }
+    }
+
+    fn roll_row(self, row: usize) -> Self {
+        let width = self.width();
+
+        (0..width).fold(self, |grid, pass| grid.roll_row_pass(row, pass))
+    }
+    fn roll_row_pass(mut self, row: usize, pass: usize) -> Self {
+        let width = self.width();
+
+        for j in 0..width - pass - 1 {
+            let tile_a = self[(row, j)];
+            let tile_b = self[(row, j + 1)];
+            if sort_tiles(&tile_a, &tile_b) == Ordering::Greater {
+                // swap the value of tiles a and b
+                self[(row, j)] = tile_b;
+                self[(row, j + 1)] = tile_a;
+            }
+        }
+
+        self
+    }
+
+    fn roll_west(self) -> Self {
+        (0..self.height()).fold(self, |grid, row| {
+            grid.roll_row(row)
+        })
+    }
+    fn roll_east(self) -> Self {
+        self.reverse().roll_west().reverse()
+    }
+    fn roll_north(self) -> Self {
+        self.transpose().roll_west().transpose()
+    }
+    fn roll_south(self) -> Self {
+        self.reverse().transpose().roll_west().transpose().reverse()
+    }
+
+    fn cycle(self) -> Self {
+        self.roll_north().roll_west().roll_south().roll_east()
+    }
+
+    fn grid_load(&mut self) -> u128 {
+        (0..self.height())
+            .map(|row| self.count_rolling(row) * (self.width() - row))
+            .sum::<usize>() as u128
+    }
+    fn count_rolling(&self, row: usize) -> usize {
+        (0..self.width())
+            .filter(|col| self[(row, *col)] == Tile::Rolling)
+            .count()
+    }
+    fn string(&self) -> String{
+        // Convert self to a string by iterating over height and width
+        // and appending the character representation of each tile
+        let mut string = String::new();
+
+        for j in 0..self.height() {
+            for i in 0..self.width() {
+                string.push(match self[(j,i)] {
+                    Tile::Empty => '.',
+                    Tile::Static => '#',
+                    Tile::Rolling => 'O',
+                });
+            }
+            string.push('\n');
+        }
+
+        string
+    }
+}
+type Coordinate = (usize, usize);
+impl Index<Coordinate> for Grid {
+    type Output = Tile;
+
+    fn index(&self, index: Coordinate) -> &Self::Output {
+        let (j,i) = index;
+
+        let (j,i) = match (self.transposed, self.reversed){
+            (false, false) => (j,i),
+            (false, true) => (j, self.width() - i - 1),
+            (true, false) => (i,j),
+            (true, true) => (self.width() - i - 1, j),
+        };
+
+        &self.tiles[j][i]
+    }
+}
+impl IndexMut<Coordinate> for Grid {
+    fn index_mut(&mut self, index: Coordinate) -> &mut Self::Output {
+        let (j,i) = index;
+
+        let (j,i) = match (self.transposed, self.reversed){
+            (false, false) => (j,i),
+            (false, true) => (j, self.width() - i - 1),
+            (true, false) => (i,j),
+            (true, true) => (self.width() - i - 1, j),
+        };
+
+        &mut self.tiles[j][i]
+    }
+}
 fn parse_file(file: &str) -> Grid {
     let contents = std::fs::read_to_string(file).expect("Something went wrong reading the file");
 
-    contents
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|c| match c {
-                    '.' => Tile::Empty,
-                    '#' => Tile::Static,
-                    'O' => Tile::Rolling,
-                    _ => panic!("Invalid character in input file"),
-                })
-                .collect::<Vec<Tile>>()
-        })
-        .collect::<Vec<Vec<Tile>>>()
+    parse_string(contents)
+}
+fn parse_string(string: String) -> Grid{
+    let tiles = string.lines().map(parse_line).collect::<Vec<Vec<Tile>>>();
+    let transposed = false;
+    let reversed = false;
+
+    Grid {
+        tiles,
+        transposed,
+        reversed
+    }
+}
+fn parse_line(line: &str) -> Vec<Tile> {
+    line.chars().map(Tile::from).collect::<Vec<Tile>>()
 }
 
 fn sort_tiles(a: &Tile, b: &Tile) -> Ordering {
@@ -193,19 +247,40 @@ mod tests {
     fn test_parse_file() {
         let grid = parse_file("resources/2023/day14/test_input");
 
-        grid.iter().for_each(|row| {
+        grid.tiles.iter().for_each(|row| {
             println!("{:?}", row);
         });
     }
 
     #[test]
     fn test_roll_tiles() {
-        let grid = parse_file("resources/2023/day14/test_input");
-        let rolled_grid = roll_north(grid);
+        let grid = parse_file("resources/2023/day14/test_input").roll_north();
 
-        rolled_grid.iter().for_each(|row| {
+        grid.tiles.iter().for_each(|row| {
             println!("{:?}", row);
         });
+        println!();
+
+        let grid = parse_file("resources/2023/day14/test_input").roll_south();
+
+        grid.tiles.iter().for_each(|row| {
+            println!("{:?}", row);
+        });
+        println!();
+
+        let grid = parse_file("resources/2023/day14/test_input").roll_west();
+
+        grid.tiles.iter().for_each(|row| {
+            println!("{:?}", row);
+        });
+        println!();
+
+        let grid = parse_file("resources/2023/day14/test_input").roll_east();
+
+        grid.tiles.iter().for_each(|row| {
+            println!("{:?}", row);
+        });
+        println!();
     }
 
     #[test]
@@ -215,19 +290,15 @@ mod tests {
 
     #[test]
     fn test_cycle() {
-        let grid = parse_file("resources/2023/day14/test_input");
-        let cycled_grid = cycle(grid);
+        let grid = parse_file("resources/2023/day14/test_input").cycle();
 
-        cycled_grid.iter().for_each(|row| {
+        grid.tiles.iter().for_each(|row| {
             println!("{:?}", row);
         });
     }
 
-    #[test]
-    fn test_billion_cycles() {
-        assert_eq!(
-            billion_cycles_load("resources/2023/day14/test_input"),
-            64
-        );
-    }
+        #[test]
+        fn test_billion_cycles() {
+            assert_eq!(billion_cycles_load("resources/2023/day14/test_input"), 64);
+        }
 }
